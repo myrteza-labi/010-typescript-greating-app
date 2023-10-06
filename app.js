bd79e098989bb0316227450a287b2dd9a986535a
myrteza <myrteza@hotmail.fr>
2023-10-06 22:08:55 +0200

    project done 90% autonomus

---
+++ app.js
@@ -0,0 +1,6 @@
+var titleText = "App typescript message simple - Entrainement fullstack n°10";
+var messageText = "Ceci est un message";
+var title = document.getElementById('title');
+var message = document.getElementById('message');
+title && (title.textContent = titleText);
+message && (message.textContent = messageText);
